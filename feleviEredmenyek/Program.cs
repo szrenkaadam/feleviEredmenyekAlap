17434d0b75d1a63c6a06866c8c96b94aae8cdbb7
Ádám Szrenka <szrenka.adam@gmail.com>
2023-11-13 10:44:22 +0100

    Add project files.

---
+++ feleviEredmenyek/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace FeleviEredmenyek
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Tanulo> tanulok = new List<Tanulo>();
+
+            List<string> tantargyak;
+            using (var sr = new StreamReader(@"..\..\..\src\tanulok.txt", Encoding.UTF8))
+            {
+                tantargyak = sr.ReadLine().Split("\t").Skip(2).ToList();
+                while (!sr.EndOfStream)
+                {
+                    sr.ReadLine();
+                    tanulok.Add(new Tanulo(sr.ReadLine(), tantargyak));
+
+                }
+
+            }
+            for (int i = 0; i < tanulok.Count; i++)
+            {
+                Console.WriteLine($"{tanulok[i].Jegyek.Average}");
+            }
+
+
+
+
+        }
+    }
+}
---
+++ feleviEredmenyek/Tanulo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace FeleviEredmenyek
+{
+    class Tanulo
+    {
+        private List<string> tantargyak;
+        public string TanuloNev { get; set; }
+        public string OktatasiAzonosito { get; set; }
+        public List<int> Jegyek;
+
+
+        public Tanulo(string r, List<string> tantargyak)
+        {
+            string[] s = r.Split("\t");
+            TanuloNev = s[0];
+            OktatasiAzonosito = s[1];
+            Jegyek = new List<int>();
+            for (int i = 2; i < s.Length; i++)
+            {
+                Jegyek.Add(int.Parse(s[i]));
+            }
+            tantargyak = tantargyak;
+        }
+
+
+        public override string ToString()
+        {
+            string osztalyzat = $"Tanuló Neve: {TanuloNev}, Oktatási azonosito: {OktatasiAzonosito}, Jegyek:  ";
+            for (int i = 0; i < Jegyek.Count; i++)
+            {
+                osztalyzat += "\n";
+                osztalyzat += tantargyak[i] + " " + Jegyek[i];
+
+            }
+            return osztalyzat;
+        }
+    }
+}
